ccf18488765ba82984ab85a1450b611a92e1971e
Maksim Horbachevsky <fantactuka@gmail.com>
2013-12-11 10:52:07 +0300

    Updating to 0.2.3

--- backbone-validator.js
+++ backbone-validator.js
@@ -21,7 +21,7 @@
 
   var Validator = Backbone.Validator = {
 
-    version: '0.2.2',
+    version: '0.2.3',
 
     /**
      * General validation method that gets attributes list and validations config and runs them all
